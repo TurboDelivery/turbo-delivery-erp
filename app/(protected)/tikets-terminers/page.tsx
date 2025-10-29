12d73487f6a813a275e1ec5b53140fbc4bfa077d
Yamoussa Keita <yamoussa.keita@lunion-lab.com>
2025-10-29 07:52:04 +0000

    modified:   app/(protected)/tikets-terminers/page.tsx

--- app/(protected)/tikets-terminers/page.tsx
+++ app/(protected)/tikets-terminers/page.tsx
@@ -5,7 +5,7 @@ import { getBonLivraisonAll } from "@/src/actions/bon-commande.action";
 
 
 export default async function Page() {
-    const data = await getBonLivraisonAll(0, 10);
+    const data = await getBonLivraisonAll(0, 10, { dates: { start: null, end: null } });
     const restaurants = await getAllRestaurants();
     return (
         <Content initialData={data} restaurants={restaurants} />
