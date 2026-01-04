9795897bd8d5e3a27afe95283f66a02d47b3c474
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2026-01-04 18:26:38 +0000

    Add collapsible component and ticket statistics features; implement infinite scroll and update ticket filters

--- features/tickets/request/tickets.request.ts
+++ features/tickets/request/tickets.request.ts
@@ -1,6 +1,6 @@
 import { BonLivraisonTerminee } from '@/types/bon-livraison.model';
 import { apiClientHttp } from '@/lib/api-client-http';
-import { ITicketParams } from '@/features/tickets/types/tickets.type';
+import { ILivreurTicket, ITicketParams, ITicketsStats } from '@/features/tickets/types/tickets.type';
 import { PaginatedResponse } from '@/types/general';
 import { BonLivraisonTermineeSchema, BonLivraisonTermineeType } from '../schema/ticket.schema';
 
@@ -10,34 +10,58 @@ const bonLivraisonEndpoints = {
   getBonLivraisonAll: {
     endpoint: `${BASE_URL}/tous`,
     method: 'GET',
-    
   },
   bonLivraisonTerminers: { endpoint: `${BASE_URL}/tous-termines`, method: 'GET' },
   bonLivraisonTerminees: { endpoint: `${BASE_URL}/tous/termines`, method: 'GET' },
   bonLivraisonEnAttentes: { endpoint: `${BASE_URL}/tous-attentes`, method: 'GET' },
+  listeLivreursTickets: { endpoint: `${BASE_URL}/livreurs/tickets`, method: 'GET' },
+  stats: { endpoint: `${BASE_URL}/stats`, method: 'GET' },
   creerBonLivraison: { endpoint: `${BASE_URL}/create`, method: 'POST' },
   supprimerBonLivraison: { endpoint: `${BASE_URL}/supprimer`, method: 'DELETE' },
 };
 
 export async function getBonLivraisonRequest(params: ITicketParams): Promise<PaginatedResponse<BonLivraisonTerminee>> {
-  try {
-    console.log('Fetching BonLivraison with params:', params);
-    return await apiClientHttp.request<PaginatedResponse<BonLivraisonTerminee>>({
-      endpoint: bonLivraisonEndpoints.bonLivraisonTerminees.endpoint,
-      method: bonLivraisonEndpoints.bonLivraisonTerminees.method,
-      params: {
-        page: params.page?.toString() || '0',
-        size: params.size?.toString() || '10',
-        restaurantId: params.restaurantId,
-        livreurId: params.livreurId,
-        debut: params.debut?.toISOString()?.split('T')?.[0],
-        fin: params.fin?.toISOString()?.split('T')?.[0],
-        search: params.search,
-      },
-    });
-  } catch (error) {
-    return [] as any;
-  }
+  return await apiClientHttp.request<PaginatedResponse<BonLivraisonTerminee>>({
+    endpoint: bonLivraisonEndpoints.bonLivraisonTerminees.endpoint,
+    method: bonLivraisonEndpoints.bonLivraisonTerminees.method,
+    params: {
+      page: params.page?.toString() || '0',
+      size: params.size?.toString() || '10',
+      restaurantId: params.restaurantId,
+      livreurId: params.livreurId,
+      debut: params.debut?.toISOString()?.split('T')?.[0],
+      fin: params.fin?.toISOString()?.split('T')?.[0],
+      search: params.search,
+    },
+  });
+}
+
+export async function getBonLivraisonStatsRequest(params: ITicketParams) {
+  return await apiClientHttp.request<ITicketsStats>({
+    endpoint: bonLivraisonEndpoints.stats.endpoint,
+    method: bonLivraisonEndpoints.stats.method,
+    params: {
+      search: params.search,
+      restaurantId: params.restaurantId,
+      livreurId: params.livreurId,
+      debut: params.debut?.toISOString()?.split('T')?.[0],
+      fin: params.fin?.toISOString()?.split('T')?.[0],
+    },
+  });
+}
+
+export async function getLivreursWithTicketsRequest(params: ITicketParams) {
+  return await apiClientHttp.request<PaginatedResponse<ILivreurTicket>>({
+    endpoint: bonLivraisonEndpoints.listeLivreursTickets.endpoint,
+    method: bonLivraisonEndpoints.listeLivreursTickets.method,
+    params: {
+      search: params.search,
+      restaurantId: params.restaurantId,
+      livreurId: params.livreurId,
+      debut: params.debut?.toISOString()?.split('T')?.[0],
+      fin: params.fin?.toISOString()?.split('T')?.[0],
+    },
+  });
 }
 
 export async function createBonLivraisonRequest(data: BonLivraisonTermineeType) {
--- features/tickets/types/tickets.type.ts
+++ features/tickets/types/tickets.type.ts
@@ -1,3 +1,5 @@
+import { Ticket } from '@/types/bon-livraison.model';
+
 import { BonLivraisonTerminee } from "@/types/bon-livraison.model";
 
 export interface ITicketParams {
@@ -11,6 +13,33 @@ export interface ITicketParams {
   tab?: 'tous' | 'termines' | 'attentes';
   livreur?: string;
 }
+
+export interface ITicketsStats {
+  revenus: number;
+  tickets: number;
+  livreurs: number;
+  restaurants: number;
+}
+
+export interface newTicket extends Ticket {
+  coutCommande: number;
+}
+
+export interface TicketsJour {
+  jour: string;
+  montantTotal: number;
+  tickets: newTicket[];
+}
+
+export interface ILivreurTicket {
+  id: string;
+  nom: string;
+  prenom: string;
+  tickets: TicketsJour[];
+}
+
+
+
 interface ITicketResponse {
   data: BonLivraisonTerminee[];
   total: number;
