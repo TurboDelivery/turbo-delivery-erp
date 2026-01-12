f16e47d83c5f3d5d14e0c2c790fe4e2e4359da2c
Yamoussa Keita <yamoussa.keita@lunion-lab.com>
2026-01-12 15:37:25 +0000

    modified:   app/(protected)/tickets/content.tsx 	modified:   app/(protected)/tickets/page.tsx

--- app/(protected)/tickets/page.tsx
+++ app/(protected)/tickets/page.tsx
@@ -1,8 +1,10 @@
 import Content from './content';
+import { getProfile } from '@/src/actions/users.actions';
 import { getAllRestaurants } from '@/src/restaurants/restaurants.actions';
 
 export default async function Page() {
+  const profile = await getProfile();
   const restaurants = await getAllRestaurants();
 
-  return <Content restaurants={restaurants} />;
+  return <Content restaurants={restaurants} profile={profile} />;
 }
