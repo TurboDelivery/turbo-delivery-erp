c4ac38237e1af213cb58f4edf35958cbea345789
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2026-01-26 11:23:28 +0000

    feat: update ticket statistics query to use structured search parameters

--- features/tickets/hooks/use-tickets-stats.ts
+++ features/tickets/hooks/use-tickets-stats.ts
@@ -1,12 +1,24 @@
 import { useTicketsStatsQuery } from '@/features/tickets/queries/ticket-stats.query';
 import { useTicketFilters } from '@/features/tickets/hooks/use-ticket-filters';
+import { ITicketParams } from '@/features/tickets/types/tickets.type';
 
 export function useTicketsStats() {
   const { filters } = useTicketFilters();
-  const { data, isLoading, isError, error } = useTicketsStatsQuery(filters);
+
+  const currentSearchParams: ITicketParams = {
+    page: filters.page,
+    size: filters.size,
+    search: filters.search,
+    livreurId: filters.livreurId,
+    restaurantId: filters.restaurantId,
+    debut: filters.debut,
+    fin: filters.fin,
+  };
+
+  const { data, isLoading, isError, error } = useTicketsStatsQuery(currentSearchParams);
 
   return {
-    ticketsStats : {
+    ticketsStats: {
       totalRevenus: data?.revenus || 0,
       totalTickets: data?.tickets || 0,
       totalLivreurs: data?.livreurs || 0,
