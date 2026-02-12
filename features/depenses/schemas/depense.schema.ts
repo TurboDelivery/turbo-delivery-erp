4916ce324e811f6f8468153e702d9b2d5c8865cb
Kouakou Christ Axel <kouakoucaxel@gmail.com>
2026-02-12 13:48:06 +0000

    fix: suppression de la verification du uuid sur l investissementId

--- features/depenses/schemas/depense.schema.ts
+++ features/depenses/schemas/depense.schema.ts
@@ -4,13 +4,13 @@ export const DepenseCreateSchema = z.object({
   description: z.string().min(1, 'La description est obligatoire'),
   montant: z.number().min(1, 'Le montant doit être supérieur à 0'),
   dateDepense: z.date({ message: 'La date de dépense est obligatoire' }),
-  categorieDepense: z.string({ message: 'La catégorie est obligatoire' }).uuid('La catégorie n’est pas valide'),
+  categorieDepense: z.string({ message: 'La catégorie est obligatoire' }),
   sourcePaiement: z
     .string({
       message: 'La source est obligatoire',
     })
     .min(1, 'La source obligatoire'),
-  investissementId: z.string({ message: "L'investissement n'est pas valide" }).uuid({ message: "Cet investissement n'est pas reconnu" }).optional(),
+  investissementId: z.string({ message: "L'investissement n'est pas valide" }).optional(),
 });
 
 export type DepenseCreateDTO = z.infer<typeof DepenseCreateSchema>;
