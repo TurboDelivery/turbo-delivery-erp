eda18a59cd077a87a489f50d93024d1ea805b210
niortien <niortiencoulibaly2001@gmail.com>
2026-03-11 13:14:23 +0000

    erreur

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
 
   },
-  // output: 'standalone', // Temporairement désactivé pour résoudre les erreurs de symlinks
+  output: 'standalone', // Réactivé pour le build Docker
   images: {
     remotePatterns: [
       {
