41d08876ae07029411b47508f443462a3245b610
niortien <niortiencoulibaly2001@gmail.com>
2026-03-26 15:49:28 +0000

    cards

--- components/personnel/request-stats.tsx
+++ components/personnel/request-stats.tsx
@@ -65,7 +65,7 @@ export function RequestStats({ pending, approved, rejected }: RequestStatsProps)
             </CardBody>
           </Card>
 
-          <Card>
+          {/* <Card>
             <CardHeader className="pb-2">
               <h4 className="text-sm font-medium text-gray-600">Demandes approuvées</h4>
             </CardHeader>
@@ -75,7 +75,7 @@ export function RequestStats({ pending, approved, rejected }: RequestStatsProps)
               </div>
               <p className="text-sm text-gray-500">Validées</p>
             </CardBody>
-          </Card>
+          </Card> */}
 
           <Card>
             <CardHeader className="pb-2">
